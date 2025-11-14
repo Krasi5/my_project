d1380911ad45c5f358bf2095b3081b2dc31a27b9
Krasi5 <krasi_todorova@yahoo.com>
2025-11-14 02:38:11 +0000

    second commit

--- her_project/settings.py
+++ her_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y#l$z&)9)1wu51*8mvw2vfo6djezrvb-zxky6tmr0w!o@j-l6x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
