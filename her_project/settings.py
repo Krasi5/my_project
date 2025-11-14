257e42085a4b79850657ea07f09a779af2b28730
Krasi5 <krasi_todorova@yahoo.com>
2025-11-14 08:43:09 +0000

    last add

--- her_project/settings.py
+++ her_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y#l$z&)9)1wu51*8mvw2vfo6djezrvb-zxky6tmr0w!o@j-l6x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
